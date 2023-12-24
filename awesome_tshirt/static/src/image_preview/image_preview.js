55e4f895c6b1c1ee0bd8f7cd241c20c6f2f7e468
Nikita Kuchev <n.kuchev@itransition.com>
2023-12-24 22:49:29 +0300

    new command

--- awesome_tshirt/static/src/image_preview/image_preview.js
+++ awesome_tshirt/static/src/image_preview/image_preview.js
@@ -4,9 +4,23 @@ import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import { CharField } from "@web/views/fields/char/char_field";
+import { useService } from "@web/core/utils/hooks";
+import { useCommand } from "@web/core/commands/command_hook";
 
 
-class ImagePreview extends Component {}
+class ImagePreview extends Component {
+    setup() {
+        this.commandService = useService("command");
+        useCommand(
+            this.env._t("Open image in new tab"),
+            () => {
+                if (this.props.value) {
+                    window.open(this.props.value, "_blank");
+                }
+            }
+        )
+    }
+}
 
 ImagePreview.template = "awesome_tshirt.ImagePreview";
 ImagePreview.components = { CharField };
